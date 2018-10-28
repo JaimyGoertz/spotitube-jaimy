54fdab53b2b014278b054c225035b34b3e885afa
JaimyGoertz <jaimygoertz@gmail.com>
2018-10-28 12:57:14 +0100

    niks

--- src/main/java/nl/han/oose/jaimy/persistence/playlist/PlaylistDAO.java
+++ src/main/java/nl/han/oose/jaimy/persistence/playlist/PlaylistDAO.java
@@ -4,17 +4,13 @@ package nl.han.oose.jaimy.persistence.playlist;
 import nl.han.oose.jaimy.entity.playlist.Playlist;
 import nl.han.oose.jaimy.entity.playlist.PlaylistOverview;
 import nl.han.oose.jaimy.entity.track.Track;
-import nl.han.oose.jaimy.entity.track.TrackOverview;
 import nl.han.oose.jaimy.persistence.ConnectionFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class PlaylistDAO {
 
@@ -110,48 +106,6 @@ public class PlaylistDAO {
             throw new RuntimeException(e);
         }
         return playlistLength;
-    }
-
-    public TrackOverview getContentOfPlaylist(int playlistId) {
-        TrackOverview trackOverview = new TrackOverview();
-        try (
-                Connection connection = connectionFactory.getConnection();
-                PreparedStatement statement = connection.prepareStatement("SELECT track.id, track.title, " +
-                        "track.performer,track.duration,track.album,track.playcount,track.publicationDate,track.description," +
-                        "track.offlineAvailable\n" +
-                        "FROM playlist_tracks INNER JOIN track ON playlist_tracks.track_id = track.id\n" +
-                        "WHERE playlist_tracks.playlist_id = ?");
-        ) {
-            ResultSet resultSet = statement.executeQuery();
-            statement.setInt(1, playlistId);
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String title = resultSet.getString("title");
-                String performer = resultSet.getString("performer");
-                int duration = resultSet.getInt("duration");
-                String album = resultSet.getString("album");
-                int playcount = resultSet.getInt("playcount");
-                String publicationDate = null;
-
-                if (!(null == (resultSet.getString("publication_date")))) {
-                    SimpleDateFormat oldFormat = new SimpleDateFormat("yyyy-MM-dd");
-                    Date date = oldFormat.parse(resultSet.getString("publication_date"));
-                    SimpleDateFormat newFormat = new SimpleDateFormat("dd-MM-yyyy");
-
-                    publicationDate = newFormat.format(date);
-                }
-
-                String description = resultSet.getString("description");
-                boolean offlineAvailable = resultSet.getBoolean("offlineAvailable");
-                trackOverview.getTracks().add(new Track(id, title, performer, duration, album, playcount, publicationDate, description, offlineAvailable));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-        return trackOverview;
-
     }
 
     public void addTrackToPlaylist(int playlistId, Track track) {
